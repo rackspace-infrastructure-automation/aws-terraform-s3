60f539ca7ed04f23a33ed32564275f00cea83c71
mlattanzi <matt.lattanzi@rackspace.com>
2018-10-24 14:55:29 -0500

    Set environment tag as specified, if no value set to empty string

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 locals {
   acl_list = ["authenticated-read", "aws-exec-read", "bucket-owner-read", "bucket-owner-full-control", "log-delivery-write", "private", "public-read", "public-read-write"]
-  env_list = ["Development", "Integration", "PreProduction", "Production", "QA", "Staging", "Test"]
 
   # In order to not have to duplicate resources w/ and w/o web config, this checks and then adds website config as needed.
   bucket_website_config = {
@@ -17,11 +16,10 @@ locals {
   # Standard tags to use and then merge with custom tags.
   default_tags = {
     ServiceProvider = "Rackspace"
-    Environment     = "${contains(local.env_list, var.environment) ? var.environment:"Development"}"
+    Environment     = "${var.environment ? var.environment:""}"
   }
 
   merged_tags = "${merge(local.default_tags, var.bucket_tags)}"
-
   # If object expiration is greater than 0 then add object expiration, otherwise do not add.
   object_expiration = {
     enabled = [{
@@ -30,9 +28,7 @@ locals {
 
     disabled = "${list()}"
   }
-
   object_expiration_config = "${var.object_expiration_days > 0 ? "enabled" : "disabled"}"
-
   # Enable bucket logging?
   bucket_logging = {
     enabled = [{
@@ -42,9 +38,7 @@ locals {
 
     disabled = "${list()}"
   }
-
   bucket_logging_config = "${var.bucket_logging ? "enabled" : "disabled"}"
-
   # Enable Noncurrent Object Version Expiration?
   noncurrent_version_expiration = {
     enabled = [{
@@ -53,9 +47,7 @@ locals {
 
     disabled = "${list()}"
   }
-
   noncurrent_version_expiration_config = "${var.noncurrent_version_expiration_days > 0 ? "enabled":"disabled"}"
-
   # Enable File Transitions?
   transition = {
     ia_enabled = [{
@@ -70,12 +62,9 @@ locals {
 
     disabled = "${list()}"
   }
-
   ia_transitions      = "${var.transition_to_ia_days > 0 ? "ia_enabled": "disabled"}"
   glacier_transitions = "${var.transition_to_glacier_days > 0 ? "glacier_enabled": "disabled"}"
-
-  transitions = "${concat(local.transition[local.ia_transitions], local.transition[local.glacier_transitions])}"
-
+  transitions         = "${concat(local.transition[local.ia_transitions], local.transition[local.glacier_transitions])}"
   noncurrent_version_transition = {
     ia_enabled = [{
       days          = "${var.noncurrent_version_transition_ia_days}"
@@ -89,12 +78,9 @@ locals {
 
     disabled = "${list()}"
   }
-
   nc_ia_transitions      = "${var.noncurrent_version_transition_ia_days > 0 ? "ia_enabled": "disabled"}"
   nc_glacier_transitions = "${var.noncurrent_version_transition_glacier_days > 0 ? "glacier_enabled":"disabled"}"
-
-  nc_transitions = "${concat(local.noncurrent_version_transition[local.nc_ia_transitions], local.noncurrent_version_transition[local.nc_glacier_transitions])}"
-
+  nc_transitions         = "${concat(local.noncurrent_version_transition[local.nc_ia_transitions], local.noncurrent_version_transition[local.nc_glacier_transitions])}"
   # Lifecycle Rules
   lifecycle_rules = {
     enabled = [
@@ -112,9 +98,7 @@ locals {
 
     disabled = "${list()}"
   }
-
   lifecycle_rules_config = "${var.lifecycle_enabled ? "enabled":"disabled"}"
-
   # CORS rules
   cors_rules = {
     enabled = [
@@ -129,9 +113,7 @@ locals {
 
     disabled = "${list()}"
   }
-
   cors_rules_config = "${length(var.allowed_origins) > 0 ? "enabled":"disabled"}"
-
   # SSE Rule Configuration
   server_side_encryption_rule = {
     enabled = [
@@ -149,7 +131,6 @@ locals {
 
     disabled = "${list()}"
   }
-
   server_side_encryption_rule_config = "${var.sse_algorithm == "none" ? "disabled" : "enabled"}"
 }
 
