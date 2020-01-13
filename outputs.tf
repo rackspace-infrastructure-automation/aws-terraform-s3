7f210ae8e408a1ca54019d0398194197142460a3
John Titus <john.titus@rackspace.com>
2020-01-13 11:25:24 -0600

    Sort outputs in alphabetical order

--- outputs.tf
+++ outputs.tf
@@ -8,14 +8,14 @@ output "bucket_domain_name" {
   value       = aws_s3_bucket.s3_bucket.bucket_domain_name
 }
 
-output "bucket_id" {
-  description = "The name of the bucket."
-  value       = aws_s3_bucket.s3_bucket.id
-}
-
 output "bucket_hosted_zone_id" {
   description = "The Route 53 Hosted Zone ID for this bucket's region."
   value       = aws_s3_bucket.s3_bucket.hosted_zone_id
+}
+
+output "bucket_id" {
+  description = "The name of the bucket."
+  value       = aws_s3_bucket.s3_bucket.id
 }
 
 output "bucket_region" {
@@ -25,12 +25,13 @@ output "bucket_region" {
 
 # endpoint and domain do not return correctly if `website = false` per https://github.com/terraform-providers/terraform-provider-aws/issues/4132
 # If you would like to use these outputs, uncomment but make sure `website = true`
-//output "bucket_website_endpoint" {
-//  description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string."
-//  value       = "${aws_s3_bucket.s3_bucket.website_endpoint}"
-//}
-//
-//output "bucket_website_domain" {
-//  description = "The domain of the website endpoint, if the bucket is configured with a website. If not, this will be an empty string. This is used to create Route 53 alias records."
-//  value       = "${aws_s3_bucket.s3_bucket.website_domain}"
-//}
+
+# output "bucket_website_domain" {
+#   description = "The domain of the website endpoint, if the bucket is configured with a website. If not, this will be an empty string. This is used to create Route 53 alias records."
+#   value       = aws_s3_bucket.s3_bucket.website_domain
+# }
+
+# output "bucket_website_endpoint" {
+#   description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string."
+#   value       = aws_s3_bucket.s3_bucket.website_endpoint
+# }
