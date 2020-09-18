a55eb4139425d6517451ca13bfb208c197f9b8b3
Giovanni Del Valle <giovanni.delvalle@rackspace.com>
2020-09-18 17:25:41 -0500

    conditionally output website related attributes

--- examples/s3.tf
+++ examples/s3.tf
@@ -14,7 +14,7 @@ resource "random_string" "s3_rstring" {
 }
 
 module "s3" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-s3//?ref=v0.12.0"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-s3//?ref=v0.12.2"
 
   bucket_logging                             = false
   bucket_acl                                 = "bucket-owner-full-control"
@@ -31,6 +31,30 @@ module "s3" {
   website                                    = true
   website_error                              = "error.html"
   website_index                              = "index.html"
+
+  tags = {
+    RightSaid = "Fred"
+    LeftSaid  = "George"
+  }
+}
+
+
+module "s3_no_website" {
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-s3//?ref=v0.12.2"
+
+  bucket_logging                             = false
+  bucket_acl                                 = "bucket-owner-full-control"
+  environment                                = "Development"
+  lifecycle_enabled                          = true
+  name                                       = "${random_string.s3_rstring.result}-example-s3-bucket-no-web"
+  noncurrent_version_expiration_days         = "425"
+  noncurrent_version_transition_glacier_days = "60"
+  noncurrent_version_transition_ia_days      = "30"
+  object_expiration_days                     = "425"
+  transition_to_glacier_days                 = "60"
+  transition_to_ia_days                      = "30"
+  versioning                                 = true
+  website                                    = false
 
   tags = {
     RightSaid = "Fred"
