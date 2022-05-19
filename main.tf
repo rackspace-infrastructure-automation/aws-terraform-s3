190a3ccade11d4dc854de5690c83ab125908f7ff
Travis Stabler <travis.stabler@rackspace.com>
2022-05-19 10:03:08 -0400

    added public blocking

--- main.tf
+++ main.tf
@@ -218,6 +218,8 @@ locals {
 
 resource "aws_s3_bucket" "s3_bucket" {
   acl           = contains(local.acl_list, var.bucket_acl) ? var.bucket_acl : "ACL_ERROR"
+  block_public_acls = var.block_public_acls
+  block_public_policy = var.block_public_policy
   bucket        = var.name
   force_destroy = var.force_destroy_bucket
   tags          = merge(var.tags, local.default_tags)
--- variables.tf
+++ variables.tf
@@ -17,10 +17,22 @@ variable "allowed_origins" {
   default     = []
 }
 
+variable "block_public_acls" {
+  description = ""
+  type        = bool
+  default     = true
+}
+
+variable "block_public_policy" {
+  description = ""
+  type        = bool
+  default     = true
+}
+
 variable "bucket_acl" {
   description = "Bucket ACL. Must be either authenticated-read, aws-exec-read, bucket-owner-read, bucket-owner-full-control, log-delivery-write, private, public-read or public-read-write. For more details https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl"
   type        = string
-  default     = "bucket-owner-full-control"
+  default     = "private"
 }
 
 variable "bucket_logging" {
